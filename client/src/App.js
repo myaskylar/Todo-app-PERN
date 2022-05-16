335b67a73c245d14738768466ad6ffaf93207d9a
Mya <myaskylar14@gmail.com>
2022-05-16 17:45:55 +0100

    connect DB to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 // import { BrowserRouter as Router } from "react-router-dom";
-import InputTask from "./components/inputTask";
 import ListTask from "./components/listTask";
-import EditTask from "./components/editTask";
 
 const App = () => (
   <div>
     <h1>hello word</h1>
-    <InputTask />
     <ListTask />
-    <EditTask />
   </div>
 );
 
--- client/src/components/listTask.js
+++ client/src/components/listTask.js
@@ -1,9 +1,34 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 
 const ListTask = () => {
+  const [allTask, setAllTask] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/task")
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error(res.statusText);
+        }
+        return res.data;
+      })
+      .then((data) => {
+        setAllTask(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div>
-      <h1> List Task </h1>
+      <h1> Task List </h1>
+      <ul>
+        {allTask.map((task, index)=> {
+         return <li key={index}>{task.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
